4d47e0359d75caa13ada226c994b6de88d5dc611
Mohamed-Amine Benali <benali.medamine2002@gmail.com>
2025-01-05 01:22:10 +0100

    fix: cleanup to the DHT/urls.py

--- backend/DHT/urls.py
+++ backend/DHT/urls.py
@@ -4,12 +4,6 @@ from rest_framework.routers import DefaultRouter
 from .viewsets.user import UserViewSet
 from .viewsets.dht import DHTViewSet
 
-# url_dhtpatterns = [
-#     path("api/dht",dht.d_list,name='json'),
-#     path("api/dht/post",dht.d_list,name='json'),
-#     path("api/dht/last-data",dht.last_data,name='json'),
-# ]
-
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='user')
 router.register('dhts', DHTViewSet, basename='DHT')
@@ -27,8 +21,4 @@ urlpatterns = [
     path('chart-data-jour/',views.chart_data_jour,name='chart-data-jour'),
     path('chart-data-semaine/',views.chart_data_semaine,name='chart-data-semaine'),
     path('chart-data-mois/',views.chart_data_mois,name='chart-data-mois'),
-    # path('', views.home, name='home'),
 ]
-
-# urlpatterns.extend(url_dhtpatterns)
-# urlpatterns.extend(url_userpatterns)
