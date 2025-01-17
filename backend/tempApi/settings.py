1dfbcfd1d5b2803ab6f34adabb6211998505435d
Mohamed-Amine Benali <benali.medamine2002@gmail.com>
2025-01-17 15:07:37 +0100

    fix(backend/CORS): modified CORS so it only allows http://localhost:5173 as origin+ allow credentials

--- backend/tempApi/settings.py
+++ backend/tempApi/settings.py
@@ -58,7 +58,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'tempApi.urls'
 
