9f2e423c190dc2a64389875b7a54864ed277783e
Mohamed-Amine Benali <benali.medamine2002@gmail.com>
2025-01-17 17:56:30 +0100

    fix(frontend): added new loading component

--- frontend/src/shared/Loading/Loading.tsx
+++ frontend/src/shared/Loading/Loading.tsx
@@ -1,5 +1,12 @@
+import { BeatLoader } from "react-spinners";
+import './Loading.css'
+
 const Loading = () => {
-    return <progress></progress>
+    return <div className="loader-container">
+        <div>
+            <BeatLoader />
+        </div>
+    </div>
 }
 
 export default Loading;
